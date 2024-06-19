74c5c283598cc25a50f2f5fca7f0ce6f75e3188d
Alina Lorman <alinalorman@mail.ru>
2024-06-19 14:59:11 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+number_of_task = 12
+number_of_hours = 1.5
+name_of_course = 'Phyton'
+time_for_one_task = (number_of_hours / number_of_task)
+print('Курс:', name_of_course + ', всего задач:' + str(number_of_task) + ', затрачено часов:', str(number_of_hours) + ', среднее время выполнения', time_for_one_task, 'часа.')
